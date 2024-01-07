17bed976e198a977fb6d8376d0a8405eddc35ed7
AramisB <brendachem7@gmail.com>
2024-01-07 10:49:16 -0800

    data structures

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    """program that prints the reverse of a list"""
-    my_list.reverse()
-
-    for integer in my_list:
-        print("{:d}".format(integer))
+    if my_list is None:
+        return
+    for i in reversed(my_list):
+        print('{:d}'.format(i))
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = my_list[:]
-
+    temp_list = my_list[:]
     if 0 <= idx < len(my_list):
-        new_list[idx] = element
-        return new_list
+        temp_list[idx] = element
+        return(temp_list)
     return(my_list)
