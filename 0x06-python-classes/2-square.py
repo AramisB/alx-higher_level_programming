35802a2e5ca1e9ca10577e6920661e5810f03c69
AramisB <brendachem7@gmail.com>
2024-01-23 08:49:57 -0800

    classes and instances

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,18 +13,13 @@ class Square:
         """
         This initializes a new instance of class Square
         Args: Size - size of the square
+        Raises:
+        TypeError: if size is not type int
+        ValueError: if size is less than 0
         """
 
         if type(size) is not int:
-            """
-            This is a TypeError exception
-            """
-
             raise TypeError("Size must be an integer.")
         if size < 0:
-            """
-            This is a ValueError exception
-            """
-
             raise ValueError("Size must be >= 0")
         self.__size = size
