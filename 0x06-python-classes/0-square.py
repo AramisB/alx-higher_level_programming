f8f556a7871591ca7e6b7c2632d2c7c16605a646
AramisB <brendachem7@gmail.com>
2024-01-23 04:12:10 -0800

    classes and instances

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+""" 
+This module defines a square class
+"""
+
 class Square:
+    """
+    This class represents a square
+    """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class
+"""
 class Square:
+    """
+    This class represents a square
+    """
     def __init__(self, size):
+        """
+        Initializes a new instance of the class Square
+        Args: __size(int) - the size of the square
+        """
         self.__size = size
