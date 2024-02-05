4e33dce0a198e3cf1013ebfa877b65e439697832
root <root@Chemu>
2024-02-05 08:01:03 -0600

    integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+A module for class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    A class that defines geometry
+    """
+    pass
+
+    def area(self):
+        """
+        A public instance
+        Raises: Exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        a public isntace that validates value
+        Raises: TypeError - if value is not an integer
+                ValueError - if value <= 0
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        self.value = value
