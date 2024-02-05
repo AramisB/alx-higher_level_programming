dd60261765949a37dd50880612831be2a16bdf0b
root <root@Chemu>
2024-02-05 07:06:19 -0600

    public instance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A module for class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    A class that defines geometry
+    """
+    pass
+
+    def area(self):
+        """
+        A public instance 
+        Raises: Exception
+        """
+        raise Exception("area() is not implemented")
