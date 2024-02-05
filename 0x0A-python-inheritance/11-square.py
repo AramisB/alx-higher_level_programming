e15d1663be699e2746d36028e1199e05cfcfffc4
root <root@Chemu>
2024-02-05 10:06:23 -0600

    square #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,55 +1,9 @@
 #!/usr/bin/python3
 """
-A module for class BaseGeometry
+A module that defines class square
 """
+Rectangle = __import__('9-rectangle').Rectangle
 
-
-class BaseGeometry:
-    """
-    A class that defines geometry
-    """
-
-    def integer_validator(self, name, value):
-        """
-        a public isntace that validates value
-        Raises: TypeError - if value is not an integer
-                ValueError - if value <= 0
-        """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """
-    Defines a rectangle that inherits from BaseGeometry
-    """
-
-    def __init__(self, width, height):
-        """
-        Instatiation of a rectangle
-        args: width (int) - width of the rectangle
-              height (int) - height of the rectangle
-        """
-        super().__init__()
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """
-        A public instance that calculates the area
-        Raises: Exception
-        """
-        return (self.__width * self.__height)
-
-    def __str__(self):
-        """
-        Returns a string representation of a rectangle
-        """
-        return f"[Rectangle] {self.__width} / {self.__height}"
 
 class Square(Rectangle):
     """
@@ -63,16 +17,5 @@ class Square(Rectangle):
                 TypeError - size must be an integer
         """
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        """
-        Calculates the area of a square
-        """
-        return (self.__size ** 2)
-
-    def __str__(self):
-        """
-        Returns a string representation of a rectangle
-        """
-        return f"[Square] {self.__width} / {self.__height}"
