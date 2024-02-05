3e93066d69c4980b9fe882bf34e5be4a95e563af
root <root@Chemu>
2024-02-05 10:52:15 -0600

    invert operators

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A module that creates a class MyInt
+"""
+
+
+class MyInt(int):
+    """
+    A class that inherits from int. It's a rebel that inverts == and != opertors
+    """
+    def __eq__(self, value):
+        """
+        Overrides == opeartor with != 
+        """
+        return self.real != value
+
+    def __ne__(self, value):
+        """
+        Overrides != operator with ==
+        """
+        return self.real == value
