525fad95bc29ece6481c16f40b73092f695a577e
AramisB <brendachem7@gmail.com>
2024-02-06 05:15:47 -0600

    read a file, with

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,11 +5,7 @@ def read_file(filename=""):
     """
     A function that reads a text file and prints to stdout
     Argument: filename - name of the file. Defaults to an empty string
-    raise: ValueError if file is empty
     """
-    if not filename:
-        raise ValueError("Filename cannot be empty")
-
-    with open("my_file_0.txt", "r", encoding="utf-8") as Myfile:
+    with open(filename, encoding="utf-8") as Myfile:
         for line in Myfile:
             print(line, end="")
