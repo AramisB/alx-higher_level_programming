28fc15969a6103c80109deacbc9ef2f18626fc4c
AramisB <brendachem7@gmail.com>
2024-02-06 05:10:16 -0600

    read a file, with

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ def read_file(filename=""):
     A function that reads a text file and prints to stdout
     Argument: filename - name of the file. Defaults to an empty string
     """
-    with open("my_file_0.txt", encoding="utf-8") as Myfile:
+    with open("my_file_0.txt", "r", encoding="utf-8") as Myfile:
         for line in Myfile:
             print(line, end="")
