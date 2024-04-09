0fe47272cf3856ce91c430b45f57510676a179f8
AramisB <brendachem7@gmail.com>
2024-04-09 18:06:38 +0300

    a function that converts a number from base 10 to another base passed as argument

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -2,8 +2,6 @@
 // a function that prints the number of arguments already printed and the new argument value.
 let argNum = 0;
 exports.logMe = function (item) {
-    console.log(argNum + ': ' + item);
-    argNum++;
+  console.log(argNum + ': ' + item);
+  argNum++;
 };
-    
-    
