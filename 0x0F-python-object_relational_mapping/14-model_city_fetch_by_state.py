cd614ac60d4ff8cde2ae1df06832237c33844bf0
AramisB <brendachem7@gmail.com>
2024-04-13 14:46:17 +0300

    model_city.py that contains the class definition of a City and a script 14-model_city_fetch_by_state.py that prints all City objects from the database hbtn_0e_14_usa

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A script that deletes all State objects with a name
+containing the letter a from the database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3])
+            )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = (
+        session.query(State.name, City.id, City.name)
+        .filter(State.id == City.state_id))
+    for instance in query:
+        print(instance[0] + ": (" + str(instance[1]) + ")" + instance[2])
