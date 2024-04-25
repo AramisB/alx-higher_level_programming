f5c0c512d08e34f8c822dfa01b29ac901d1d176e
AramisB <brendachem7@gmail.com>
2024-04-25 18:48:36 +0300

    extra line

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
-# sends a GET request to the URL, and displays the body of the response
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -sX GET $1 -L
