20e3073e5beba4fe66b9256720782894a7851885
AramisB <brendachem7@gmail.com>
2024-04-25 19:36:14 +0300

    function comment

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
 
 
 def find_peak(list_of_integers):
+    """Finds the maximu element within an unsorted list"""
     if list_of_integers is None or len(list_of_integers) == 0:
         return None
     low = 0
