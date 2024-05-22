db74524e68b34fd21cb32d7477d233be1d5fb3cd
AramisB <brendachem7@gmail.com>
2024-05-22 15:47:46 +0300

    writing data to a file asynchronously

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+if (!filePath && !stringToWrite) {
+  process.exit(1);
+}
+fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
